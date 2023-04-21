b6b43c7959eeb0488623ea0011b2c30d0062a9c8
Dharani Prabhu <dharani-prabhu.sinnapalam@bmw.de>
2023-04-21 16:05:29 +0200

    added list comprehension

---
+++ list_comprehension.py
@@ -0,0 +1,42 @@
+# Motivation readable, expressive
+
+# From existing list
+def even():
+    numbers = [1,2,3,4,5,6]
+    return [x for x in numbers if x%2 == 0]
+
+# From custom iterator
+class MyIterator:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def __iter__(self):
+        self.current = self.start
+        return self
+
+    def __next__(self):
+        if self.current >= self.end:
+            raise StopIteration
+        self.current += 1
+        return self.current
+
+def odd():
+    numbers = MyIterator(0,10)
+    return [x for x in numbers if x%2==1]
+
+# From string
+def vowel_in_string(inp):
+    vowels = ['a','e','i','o','u']
+    return [c for c in inp if c in vowels]
+
+# dict comprehension
+def square():
+    numbers = MyIterator(0,10)
+    sq = {n : n*n for n in numbers}
+    return sq
+
+print(even())
+print(odd())
+print(vowel_in_string("hello"))
+print(square().get(4))
